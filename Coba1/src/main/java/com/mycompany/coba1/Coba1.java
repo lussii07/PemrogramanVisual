c01d4efc8c76c1aafc7a9293ce4683eae3cde310
lussii <122663468+lussii07@users.noreply.github.com>
2024-08-08 08:17:39 +0700

    Add files via upload
    Modul 1-3

---
+++ Coba1/src/main/java/com/mycompany/coba1/Coba1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba1;
+
+/**
+ *
+ * @author luxxzy
+ */
+public class Coba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
